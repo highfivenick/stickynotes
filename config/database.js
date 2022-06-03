6c35ba84f0ddfda969326555cfc4a207c7c3fe89
highfivenick <nickhawkins.dev@gmail.com>
2022-06-02 20:32:46 -0400

    first commit

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb+srv://highfive_nick:nick@cluster0.nowei.mongodb.net/notepad?retryWrites=true&w=majority', 
+    'dbName': 'notepad'
+};
